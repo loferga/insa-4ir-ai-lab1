1ba2f9e1327350879b0bdcee35be7516d7df2bf5
loferga <laforgemateo@gmail.com>
2025-02-13 10:46:34 +0100

    end of lab 1 (need to be finished)

--- src/search.rs
+++ src/search.rs
@@ -26,9 +26,46 @@ impl Stats {
     }
 }
 
+fn backtrack_moves_rec(current_state: Board, predecessors: &HashMap<Board, (Board, Direction)>, mut moves: Vec<Direction>) -> Vec<Direction> {
+    match predecessors.get(&current_state) {
+        None => {return moves;}
+        Some ((parent_state, m)) => {
+            moves.push(*m);
+            println!("nouvelle valeur de moves:");
+            for direction in moves.clone() {
+                println!("{}", direction);
+            }
+            return backtrack_moves_rec(*parent_state, predecessors, moves);
+        }
+    };
+}
+
+fn backtrack_moves(state: Board, predecessors: &HashMap<Board, (Board, Direction)>) -> Vec<Direction> {
+    println!("backtracking...");
+    let mut path = backtrack_moves_rec(state, predecessors, Vec::new());
+    println!("path len: {}", path.len());
+    for direction in path.clone() {
+        println!("{}", direction);
+    }
+    path.reverse();
+    return path;
+}
+
+fn check_end_search(state: Board) -> bool {
+    return state.is_valid_plan(&[]);
+}
+
 pub fn search(init_state: Board) -> (Option<Vec<Direction>>, Stats) {
     // record the start time when starting the search (so we can later the time that elapsed since)
     let start = std::time::Instant::now();
+    if (check_end_search(init_state)) {
+        // here is an example to measure the runtime and returns the statistics
+        let runtime = start.elapsed();
+        // example to construct a Stats instance
+        let stats = Stats::new(0, runtime);
+        // return the results and associated stats
+        return (Some(Vec::new()), stats);
+    }
 
     // frontier: MinHeap provide allows to store the states to explore, with associated priority
     let mut heap: MinHeap<Board> = MinHeap::new();
@@ -42,16 +79,45 @@ pub fn search(init_state: Board) -> (Option<Vec<Direction>>, Stats) {
     // keeps track all states that have been expanded
     let mut expanded: HashSet<Board> = HashSet::new();
 
-    // ...
-    // TODO: implement the search algorithm
-    // ...
+    // search algorithm implementation
+    let mut result = None;
+
+    heap.insert(init_state, 1);
+    path_costs.insert(init_state, 1);
+
+    while !heap.is_empty() {
+        let state = heap.pop().unwrap();
+        if expanded.contains(&state) { continue };
+        if (check_end_search(state)) {
+            result = Some(Vec::new());
+            break;
+        }
+        let cost = *path_costs.get(&state).unwrap();
+
+        for direction in [Direction::Up, Direction::Right, Direction::Down, Direction::Left] {
+            let apply_result = state.apply(direction);
+            match apply_result {
+                None => /*invalid direction for state*/{},
+                Some (new_state) => {
+                    path_costs.insert(new_state, cost+1);
+                    predecessors.insert(new_state, (state, direction));
+                    heap.insert(new_state, 1);
+                    if (check_end_search(new_state)) {
+                        result = Some(backtrack_moves(new_state, &predecessors));
+                        break;
+                    }
+                }
+            }
+        }
+        expanded.insert(state);
+    }
 
     // here is an example to measure the runtime and returns the statistics
     let runtime = start.elapsed();
     // example to construct a Stats instance
-    let stats = Stats::new(0, runtime);
+    let stats = Stats::new(expanded.len(), runtime);
     // return the results and associated stats
-    (todo!(), stats)
+    (result, stats)
 }
 
 #[cfg(test)]
